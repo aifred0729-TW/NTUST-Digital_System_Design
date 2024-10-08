73ecf556b879c6d1c72cab8a2e383b753a21bca6
Red Meow <44439821+aifred0729-TW@users.noreply.github.com>
2024-10-08 15:09:30 +0800

    feat : Grouped multiple variable boolean functions

--- Project02/Project02.cpp
+++ Project02/Project02.cpp
@@ -13,7 +13,7 @@ typedef struct {
     std::vector<std::pair<std::string, char>> PLA;
 } PLAdata;
 
-typedef std::vector<std::pair<std::string, std::vector<int>>> expanedBF;
+typedef std::pair<std::string, std::vector<int>> booleanFunc;
 
 void readFile(PLAdata& data) {
     using namespace std;
@@ -40,7 +40,7 @@ void readFile(PLAdata& data) {
     return;
 }
 
-void expendPLADontCare(expanedBF& data, std::string booleanFunction, int index, int isDontCare) {
+void expendPLADontCare(std::vector<booleanFunc>& data, std::string booleanFunction, int index, int isDontCare) {
 
     if (index == booleanFunction.size()) {
         data.push_back(std::pair<std::string, std::vector<int>>(booleanFunction, { 0, isDontCare }));
@@ -60,13 +60,46 @@ void expendPLADontCare(expanedBF& data, std::string booleanFunction, int index, 
 }
 
 
-void expandBooleanFunction(PLAdata data, expanedBF& initializeBF) {
+void expandBooleanFunction(PLAdata data, std::vector<booleanFunc>& initializeBF) {
     for (unsigned int i = 0; i < data.loadCount; i++) {
         if (data.PLA[i].second == '1') expendPLADontCare(initializeBF, data.PLA[i].first, 0, 0);
         if (data.PLA[i].second == '-') expendPLADontCare(initializeBF, data.PLA[i].first, 0, 1);
     }
-    for (unsigned int i = 0; i < initializeBF.size(); i++) initializeBF[i].second[0] = std::stoi(initializeBF[i].first, nullptr, 2);
-    for (unsigned int i = initializeBF.size()-1; i > 0; i--) for (unsigned int j = 0; j < i; j++) if (initializeBF[j].second > initializeBF[j + 1].second) std::swap(initializeBF[j], initializeBF[j + 1]);
+    for (unsigned int i = 0; i < initializeBF.size(); i++) {
+        initializeBF[i].second[0] = std::stoi(initializeBF[i].first, nullptr, 2);
+    }
+    for (unsigned int i = initializeBF.size() - 1; i > 0; i--) {
+        for (unsigned int j = 0; j < i; j++) {
+            if (initializeBF[j].second > initializeBF[j + 1].second) std::swap(initializeBF[j], initializeBF[j + 1]);
+        }
+    }
+    return;
+}
+
+void groupBooleanFunctions(std::vector<booleanFunc>& bf, std::vector<std::vector<booleanFunc>>& groupedBF) {
+    using namespace std;
+
+    int trueCount = 0;
+    groupedBF.resize(bf[0].first.size() + 1);
+
+    for (unsigned int i = 0; i < bf.size(); i++) {
+        for (unsigned int j = 0; j < bf[i].first.size(); j++) {
+            if (bf[i].first[j] == '1') trueCount++;
+        }
+        groupedBF[trueCount].push_back(bf[i]);
+        trueCount = 0;
+    }
+
+    return;
+}
+
+void mergeBooleanFunctions(std::vector<std::vector<booleanFunc>>& bf) {
+    using namespace std;
+
+    for (unsigned int i = 0; i < bf.size(); i++) {
+
+    }
+
     return;
 }
 
@@ -78,8 +111,14 @@ int main() {
 
     readFile(data);
 
-    expanedBF initializeBF;
+    vector<booleanFunc> initializeBF;
+
     expandBooleanFunction(data, initializeBF);
+
+    vector<vector<booleanFunc>> groupedBF;
+
+    groupBooleanFunctions(initializeBF, groupedBF);
+
 
     return 0;
 }
